58ea672773841c7be66d98d5dce6bff0ad69a166
priyapavani27 <86479562+priyapavani27@users.noreply.github.com>
2021-06-29 20:57:35 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int i,j;
+		Scanner s=new Scanner(System.in);
+		j=s.nextInt();
+		for(i=1;i<=10;i++)
+		{
+		    System.out.println(j+"*"+i+"="+(j*i));
+		}
+	}
+}
+
+
+out put:
+
+2
+2*1=2
+2*2=4
+2*3=6
+2*4=8
+2*5=10
+2*6=12
+2*7=14
+2*8=16
+2*9=18
+2*10=20
